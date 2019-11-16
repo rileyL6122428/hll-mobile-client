8d8ca4f8cc8e09568a673126ebf93ade9032ac08
Riley Littlefield <rileylittlefield@ymail.com>
2019-11-16 16:42:05 -0500

    begins track-list.component specs

--- src/app/shared/components/track-list/track-list.component.spec.ts
+++ src/app/shared/components/track-list/track-list.component.spec.ts
@@ -1,9 +1,8 @@
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { TrackListComponent } from './track-list.component';
 
-describe('TrackListComponent', () => {
+fdescribe('TrackListComponent', () => {
   let component: TrackListComponent;
   let fixture: ComponentFixture<TrackListComponent>;
 
@@ -23,5 +22,58 @@ describe('TrackListComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  describe('null track list', () => {
+    let skeletonData: null[];
+
+    beforeEach(() => {
+      skeletonData = [null, null, null];
+      component.tracks = skeletonData;
+      fixture.detectChanges();
+    });
+
+    it('renders skeleton text for each null track', () => {
+      const contentElements = fixture
+        .debugElement
+        .nativeElement
+        .querySelectorAll('ion-item-sliding ion-item ion-skeleton-text');
+
+      expect(contentElements.length).toBe(3);
+    });
+
+    it('does not emit a play event when selected track is null', () => {
+      const emitPlaySpy = spyOn(component.play, 'next');
+
+      const playButtons = fixture
+        .debugElement
+        .nativeElement
+        .querySelectorAll('ion-item-sliding ion-item-options[start] ion-item-option');
+
+      Array
+        .from(playButtons)
+        .forEach((playButtonElement: HTMLElement) => {
+          playButtonElement.click();
+          fixture.detectChanges();
+          expect(emitPlaySpy).not.toHaveBeenCalled();
+        });
+    });
+
+    it('does not emit a delete event when selected track is null', () => {
+      const emitDeleteSpy = spyOn(component.delete, 'next');
+
+      const deleteButtons = fixture
+        .debugElement
+        .nativeElement
+        .querySelectorAll('ion-item-sliding ion-item-options[end] ion-item-option');
+
+      Array
+        .from(deleteButtons)
+        .forEach((playButtonElement: HTMLElement) => {
+          playButtonElement.click();
+          fixture.detectChanges();
+          expect(emitDeleteSpy).not.toHaveBeenCalled();
+        });
+    });
   });
 });

596804855d1a168e5e4b9f6847341470118b2006
Riley Littlefield <rileylittlefield@ymail.com>
2019-12-15 14:32:20 -0500

    refactors new-track page

--- src/app/new-track/new-track.page.ts
+++ src/app/new-track/new-track.page.ts
@@ -3,8 +3,9 @@ import { TrackHttpClient } from 'hll-shared-client';
 import { Router } from '@angular/router';
 import { AuthService } from '../shared/auth/auth.service';
 import { LoadingController, ToastController, AlertController } from '@ionic/angular';
-import { of } from 'rxjs';
-import { catchError, delay, tap } from 'rxjs/operators';
+import { of, timer, zip } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'hll-new-track',
@@ -34,66 +35,78 @@ export class NewTrackPage {
   }
 
   submit(): void {
+      this.showLoader({
+        then: () => this.uploadTrack()
+      });
+  }
+
+  private showLoader(params: { then: () => void }) {
     this.loadingController.create({
       message: 'Please wait.',
       spinner: 'bubbles'
     })
       .then((loader) => {
         loader.present();
-        this.uploadTrack();
+        params.then();
       });
   }
 
   private uploadTrack(): void {
-    this.trackClient.upload({
+    const upload$ = this.trackClient.upload({
       name: this.trackName,
       contents: this.trackContents,
       bearerToken: this.auth.idToken
     })
-
       .pipe(
-        catchError((error) => of({ errorOccurred: true, error })),
-        delay(1000),
-        tap((piped) => {
-          if (piped.errorOccurred) {
-            throw piped.error;
-          }
-        })
-      )
+        catchError((error) => of({ errorOccurred: true, error }))
+      );
 
+    const minDelay$ = timer(environment.minUploadTrackDelay);
+
+    zip(upload$, minDelay$)
       .subscribe(
-        () => {
+        ([upload]) => {
           this.loadingController.dismiss();
 
-          this.toastController.create({
-            message: `${this.trackName} was created`,
-            duration: 3000,
-            color: 'dark',
-            cssClass: 'center-text'
-          })
-            .then((toast) => {
-              toast.present();
-              this.router.navigate(['/profile'])
+          if (!upload.errorOccurred) {
+            this.showSuccessToast({
+              then: () => this.router.navigate(['/profile'])
             });
-        },
-
-        () => {
-          this.loadingController.dismiss();
-
-          this.alertController.create({
-            header: 'An error occurred.',
-            message: `We're sorry, we were unable to upload your track. Please try again at a later time.`,
-            buttons: [
-              {
-                text: 'OK',
-                role: 'cancel',
-                handler: () => this.router.navigate(['/profile'])
-              }
-            ]
-          })
-            .then((alert) => alert.present());
+          } else {
+            this.showFailureAlert({
+              onClose: () => this.router.navigate(['/profile'])
+            });
+          }
         }
       );
+  }
+
+  private showSuccessToast(params: { then: () => void }): void {
+    this.toastController.create({
+      message: `${this.trackName} was created`,
+      duration: 3000,
+      color: 'dark',
+      cssClass: 'center-text'
+    })
+      .then((toast) => {
+        toast.present();
+        params.then();
+      });
+  }
+
+  private showFailureAlert(params: { onClose: () => void }): void {
+    this.alertController.create({
+      header: 'An error occurred.',
+      message: `We're sorry, we were unable to upload your track. Please try again at a later time.`,
+      buttons: [
+        {
+          text: 'OK',
+          role: 'cancel',
+          handler: params.onClose
+        }
+      ]
+    })
+      .then((alert) => alert.present());
   }
 
   onFileChange(file: File): void {
